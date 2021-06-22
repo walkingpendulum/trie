6ab1099440f60915d510ee7666c0ff345144f432
walkingpendulum <oayunin@gmail.com>
2021-06-22 14:38:12 +0300

    add_one and add_many implemented

--- tests/test_trie_creation.py
+++ tests/test_trie_creation.py
@@ -1,6 +1,49 @@
-from trie.core import Trie
+from trie.core import Trie, Sentinel
 
 
-def test_empty_trie_created():
+def test_linear_added():
     trie = Trie()
-    assert Trie
+    trie.add_one(["a", "b", "c"])
+    assert trie.as_tree() == {"a": {"b": {"c": Sentinel}}}
+
+
+def test_cherry_added():
+    trie = Trie()
+    trie.add_one(["root", "cherry1"])
+    trie.add_one(["root", "cherry2"])
+    assert trie.as_tree() == {"root": {"cherry1": Sentinel, "cherry2": Sentinel}}
+
+
+def test_add_empty():
+    trie = Trie()
+    trie.add_one([])
+    assert trie.as_tree() == {}
+
+
+def test_add_many():
+    trie = Trie()
+    trie.add_many([
+        ["mother", "son"],
+        ["mother", "daughter"],
+    ])
+    assert trie.as_tree() == {"mother": {"son": Sentinel, "daughter": Sentinel}}
+
+
+def test_add_many_with_the_same_prefix_root_level():
+    trie = Trie()
+    trie.add_many([
+        [1],
+        [2],
+    ])
+    assert trie.as_tree() == {1: Sentinel, 2: Sentinel}
+
+
+def test_add_many_with_the_same_prefix():
+    trie = Trie()
+    trie.add_many([
+        ["grandfather", "father"],
+        ["grandfather", "father", "son"],
+    ])
+    assert trie.as_tree() == {"grandfather": {"father": {"": Sentinel, "son": Sentinel}}}
+
+
--- trie/core.py
+++ trie/core.py
@@ -1,3 +1,55 @@
-class Trie(object):
+from __future__ import annotations
+
+from itertools import chain, islice
+from typing import List, Hashable, Dict, Union, cast
+
+TreeType = Union['TreeType', Dict[Hashable, 'TreeType']]
+
+
+class Sentinel:
     pass
 
+
+class Trie(object):
+    """Implements prefix tree data structure (see https://en.wikipedia.org/wiki/Trie)"""
+    def __init__(self):
+        self._tree = {}
+
+    def add_one(self, to_add: List[Hashable]) -> 'Trie':
+        """Add one element. Input should be a list of tokens (all tokenization work should be already performed)."""
+        if not to_add:
+            return self
+
+        prev_subtree, subtree = None, self._tree
+        it = zip(chain([Sentinel], to_add), to_add, islice(chain(to_add, [Sentinel]), 1, None))
+        for prev_el, el, next_el in it:
+            if self.is_sentinel(next_el):
+                # this is the last element to add
+                if self.is_sentinel(subtree):
+                    # this is a special case: add_one(["a"]), then add_one(["a", "b]); and el == "b" right now
+                    # lets patch subtree to be {"": Sentinel} first
+                    subtree = {"": Sentinel}
+                    prev_subtree[prev_el] = subtree      # prev_subtree is always not None here!     # noqa
+
+                subtree[el] = Sentinel
+                break
+
+            if el not in subtree:
+                subtree[el] = {}
+
+            prev_subtree, subtree = subtree, subtree[el]
+
+        return self
+
+    def add_many(self, many_to_add: List[List[Hashable]]) -> 'Trie':
+        """Add many elements. Shortcut for multiple add_one calls."""
+        for to_add in many_to_add:
+            self.add_one(to_add)
+
+        return self
+
+    def as_tree(self) -> TreeType:
+        return self._tree
+
+    def is_sentinel(self, obj) -> bool:
+        return obj == Sentinel
